2a424955dd6637c5306f8105abd6a61847bde930
Fan Luo <fluo4@stevens.edu>
2022-08-26 20:04:30 -0400

    Add files via upload

---
+++ Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by 罗帆 on 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    //State allow to change variables inside struct
+    @State var leftDiceNumber = 1;
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
